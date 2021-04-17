57ce06d61b4538f086c9690c9dc232469f3d1d6a
Shikhar03Stark <hv.harshit321@gmail.com>
2021-04-17 22:23:00 +0530

    Swagger-Doc setup for route /v1

--- Server/routes/v1.js
+++ Server/routes/v1.js
@@ -1,4 +1,33 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    openapi : '3.0.0',
+    info : {
+        title : 'Mersenne REST API docs',
+        version : '1.0.0',
+        description : 'Mersenne REST API server v1',
+        contact : {
+            name : 'Mersenne',
+            email : 'hv.harshit321@gmail.com',
+        },
+    },
+    servers : [{
+        url : process.env.ROOT,
+        description : 'Master REST server',
+    }]
+};
+
+const options = {
+    swaggerDefinition,
+    apis : ['./auth.js', './v1.js', './v1/*.js'],
+};
+
+const swaggerSpecs = swaggerJSDoc(options);
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerSpecs));
 
 router.get('/', (req, res) => {
     res.json({
